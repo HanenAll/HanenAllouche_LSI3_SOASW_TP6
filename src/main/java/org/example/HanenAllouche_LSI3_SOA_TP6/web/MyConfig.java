1b25c5b8f2ca915f5c18d27e0f0b8f753791229b
Hanen Allouche <106692503+HanenAll@users.noreply.github.com>
2023-12-01 18:56:49 +0100

    upload files

--- src/main/java/org/example/HanenAllouche_LSI3_SOA_TP6/web/MyConfig.java
+++ src/main/java/org/example/HanenAllouche_LSI3_SOA_TP6/web/MyConfig.java
@@ -4,9 +4,10 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration //on l'a utilisé pour indiquer que la classe Myconfig joue le rôle de configuration dans l'application.
 public class MyConfig {
-    @Bean
+    @Bean // c'est une annotation de Spring indique que la methode resourceConfig retourne une jersyServlet
+// qui doit être géré par le conteneur Spring, et cet objet  doit être enregistré en tant que bean.
   public ResourceConfig resourceConfig(){
       ResourceConfig jersyServlet = new ResourceConfig();
       jersyServlet.register(CompteRestJAXSAPI.class);
